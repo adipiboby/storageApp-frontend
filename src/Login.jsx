a74e7c68335c6d38d72dc428d1dffbf568efa05a
Adipi Manojkumar <adipimanojkumar@gmail.com>
2026-02-14 17:52:19 +0530

    add email in login

--- src/Login.jsx
+++ src/Login.jsx
@@ -6,7 +6,7 @@ import { loginUser } from "./api/userApi";
 
 const Login = () => {
   const [formData, setFormData] = useState({
-    email: "procodrr@gmail.com",
+    email: "adipimanojkumar@gmail.com",
     password: "abcd",
   });
   const [serverError, setServerError] = useState("");
